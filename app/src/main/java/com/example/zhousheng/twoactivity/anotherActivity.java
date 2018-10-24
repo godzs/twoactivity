5463fbbd2de520d760eb53fbf768499c2e881c78
zhousheng <cwxxstar@126.com>
2018-10-24 21:07:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhousheng/twoactivity/anotherActivity.java
@@ -0,0 +1,39 @@
+package com.example.zhousheng.twoactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class anotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Button button3=(Button)findViewById(R.id.button2);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                 Intent intent = new Intent(anotherActivity.this, MainActivity.class);
+                  startActivity(intent);
+                  finish();
+                  }
+                  });
+    }
+
+}
